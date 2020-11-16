0c6f02eff8ef9f2400171bed7f668106010acee3
bradylangdale <bradylangdale@gmail.com>
2020-11-16 06:20:37 -0600

    Primitive Particle System

--- __init__.py
+++ __init__.py
@@ -4,4 +4,5 @@ from AggiEngine.statemanager import StateManager
 from AggiEngine.uimanager import UiManager
 from AggiEngine.gameobject import GameObject
 from AggiEngine.mainwindow import MainWindow
+from AggiEngine.particles import Particles
 from AggiEngine.application import Application
