93c2218ed15866c31a14dcec081afc3f0ab1280b
Nurivan <nurivan.gomez@gmail.com>
2021-04-12 21:20:44 -0500

    Added some comments

--- AggiEngine/sound.py
+++ AggiEngine/sound.py
@@ -5,13 +5,29 @@ class Sound():
         self.sounds = dict()
     
     def load(self, key, filestr):
+        """
+        Loads a wav file for future playback
+        :param key: A key you will use to access the audio playback
+        :param filestr: The file path for a wav file to play
+        """
         self.sounds[key] = sa.WaveObject.from_wave_file(filestr)
     
     def play(self, key):
+        """
+        Plays your sound
+        :param key: The key used when loading the sound
+        """
         self.sounds[key].play()
     
     def stop_all(self):
+        """
+        Stops all sounds from playing
+        """
         sa.stop_all()
     
     def stop(self, key):
+        """
+        Stops the selected sound from playing
+        :param key: The key you used when loading the sound
+        """
         self.sounds[key].stop()
